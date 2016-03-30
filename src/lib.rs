ce00d8a87b2e86ffd64b36a67affc566777a7bbc
John Ledbetter <john@callrail.com>
2016-03-30 11:43:44 -0400

    move speakers to submodule.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ extern crate memmap;
 extern crate byteorder;
 
 pub mod error;
+pub mod speakers;
 
 pub use self::error::WaveError;
+pub use self::speakers::SpeakerPosition;
 
 use std::io::{Seek,SeekFrom,Cursor};
 use memmap::{Mmap,Protection};
@@ -20,49 +22,6 @@ const FACT : u32 = 0x74636166;
 const FORMAT_PCM  : u16 = 1;
 const FORMAT_IEEE : u16 = 3;
 const FORMAT_EXT  : u16 = 0xfffe;
-
-const SPEAKER_FRONT_LEFT            : isize = (1 << 0);
-const SPEAKER_FRONT_RIGHT           : isize = (1 << 1);
-const SPEAKER_FRONT_CENTER          : isize = (1 << 2);
-const SPEAKER_LOW_FREQUENCY         : isize = (1 << 3);
-const SPEAKER_BACK_LEFT             : isize = (1 << 4);
-const SPEAKER_BACK_RIGHT            : isize = (1 << 5);
-const SPEAKER_FRONT_LEFT_OF_CENTER  : isize = (1 << 6);
-const SPEAKER_FRONT_RIGHT_OF_CENTER : isize = (1 << 7);
-const SPEAKER_BACK_CENTER           : isize = (1 << 8);
-const SPEAKER_SIDE_LEFT             : isize = (1 << 9);
-const SPEAKER_SIDE_RIGHT            : isize = (1 << 10);
-const SPEAKER_TOP_CENTER            : isize = (1 << 11);
-const SPEAKER_TOP_FRONT_LEFT        : isize = (1 << 12);
-const SPEAKER_TOP_FRONT_CENTER      : isize = (1 << 13);
-const SPEAKER_TOP_FRONT_RIGHT       : isize = (1 << 14);
-const SPEAKER_TOP_BACK_LEFT         : isize = (1 << 15);
-const SPEAKER_TOP_BACK_CENTER       : isize = (1 << 16);
-const SPEAKER_TOP_BACK_RIGHT        : isize = (1 << 17);
-const SPEAKER_RESERVED              : isize = (1 << 31);
-
-#[derive(Debug,Copy,Clone,PartialEq)]
-pub enum SpeakerPosition {
-  FrontLeft          = SPEAKER_FRONT_LEFT,
-  FrontRight         = SPEAKER_FRONT_RIGHT,
-  FrontCenter        = SPEAKER_FRONT_CENTER,
-  LowFrequency       = SPEAKER_LOW_FREQUENCY,
-  BackLeft           = SPEAKER_BACK_LEFT,
-  BackRight          = SPEAKER_BACK_RIGHT,
-  FrontLeftOfCenter  = SPEAKER_FRONT_LEFT_OF_CENTER,
-  FrontRightOfCenter = SPEAKER_FRONT_RIGHT_OF_CENTER,
-  BackCenter         = SPEAKER_BACK_CENTER,
-  SideLeft           = SPEAKER_SIDE_LEFT,
-  SideRight          = SPEAKER_SIDE_RIGHT,
-  TopCenter          = SPEAKER_TOP_CENTER,
-  TopFrontLeft       = SPEAKER_TOP_FRONT_LEFT,
-  TopFrontCenter     = SPEAKER_TOP_FRONT_CENTER,
-  TopFrontRight      = SPEAKER_TOP_FRONT_RIGHT,
-  TopBackLeft        = SPEAKER_TOP_BACK_LEFT,
-  TopBackCenter      = SPEAKER_TOP_BACK_CENTER,
-  TopBackRight       = SPEAKER_TOP_BACK_RIGHT,
-  Reserved           = SPEAKER_RESERVED,
-}
 
 #[derive(Debug,Copy,Clone,PartialEq)]
 pub enum Format {
@@ -185,39 +144,8 @@ impl WaveFile {
 
   pub fn speakers(&self) -> Option<Vec<SpeakerPosition>> {
     match self.info.channel_mask {
-      None => None,
-      Some(mask) => {
-        let mask = mask as isize;
-        let mut speakers = vec![];
-        let mut i = SPEAKER_FRONT_LEFT;
-        while i < SPEAKER_RESERVED {
-          if mask & i != 0 {
-            speakers.push(match i {
-              SPEAKER_FRONT_LEFT            => SpeakerPosition::FrontLeft,
-              SPEAKER_FRONT_RIGHT           => SpeakerPosition::FrontRight,
-              SPEAKER_FRONT_CENTER          => SpeakerPosition::FrontCenter,
-              SPEAKER_LOW_FREQUENCY         => SpeakerPosition::LowFrequency,
-              SPEAKER_BACK_LEFT             => SpeakerPosition::BackLeft,
-              SPEAKER_BACK_RIGHT            => SpeakerPosition::BackRight,
-              SPEAKER_FRONT_LEFT_OF_CENTER  => SpeakerPosition::FrontLeftOfCenter,
-              SPEAKER_FRONT_RIGHT_OF_CENTER => SpeakerPosition::FrontRightOfCenter,
-              SPEAKER_BACK_CENTER           => SpeakerPosition::BackCenter,
-              SPEAKER_SIDE_LEFT             => SpeakerPosition::SideLeft,
-              SPEAKER_SIDE_RIGHT            => SpeakerPosition::SideRight,
-              SPEAKER_TOP_CENTER            => SpeakerPosition::TopCenter,
-              SPEAKER_TOP_FRONT_LEFT        => SpeakerPosition::TopFrontLeft,
-              SPEAKER_TOP_FRONT_CENTER      => SpeakerPosition::TopFrontCenter,
-              SPEAKER_TOP_FRONT_RIGHT       => SpeakerPosition::TopFrontRight,
-              SPEAKER_TOP_BACK_LEFT         => SpeakerPosition::TopBackLeft,
-              SPEAKER_TOP_BACK_CENTER       => SpeakerPosition::TopBackCenter,
-              SPEAKER_TOP_BACK_RIGHT        => SpeakerPosition::TopBackRight,
-              _                             => unreachable!()
-            });
-          }
-          i <<= 1;
-        }
-        Some(speakers)
-      }
+      None       => None,
+      Some(mask) => Some(SpeakerPosition::decode(mask as isize))
     }
   }
 
